923a22a73a9bf3fc17afa5d48d63607d3cb1b52b
B3sTP4gAn0 <b3stp4gAn0@gmail.com>
2023-04-29 21:33:46 -0600

    .

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,6 @@ from django.urls import path
 from .views import HomeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+    path('admin/', admin.site.urls),  
     path('', HomeView.as_view(), name = "home")
 ]
